527594fa614b51d0670e9b6c55e7152e50327957
linkskk <106978598+linkskk@users.noreply.github.com>
2022-10-21 11:40:04 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,31 +2,44 @@ import random
 import numpy as np
 import matplotlib.pyplot as plt
 from numerical_methods import Numerical 
+import PIL.Image as image
+
 
 numerical = Numerical()
 methods = {"Newton": numerical.newton, "Steffensen": numerical.steffensen, 
             "Ostrowski": numerical.ostrowski, "Halley": numerical.halley}
 test_function = random.choice(numerical.list)
 
-def render(type, f, pixels=1000, xa=-1.5, xb=1.5, ya=-1.5, yb= 1.5):
+def render(type, f, pixels=1500, xa=-1.5, xb=1.5, ya=-1.5, yb= 1.5):
     print(f"Generating {type}'s fractal...")
     x = np.linspace(xa, xb, pixels)
     y = np.linspace(ya, yb, pixels)
     xx, yy = np.meshgrid(x,y)
     z = xx + yy*1j
     im = np.frompyfunc(methods[type],2, 1)(z, f).astype(float)
-    plt.imshow(im, cmap="magma")
-    plt.axis("off")
-    plt.grid(False)
-    plt.savefig("test.png",bbox_inches='tight')
+    return im
+
+def create(arr):
+    fig = plt.figure(figsize=(1500 /200.0, 1500 / 200.0), dpi=100)
+    ax = fig.add_axes([0, 0, 1, 1], aspect=1)
+    ax.axis("off")
+    ax.imshow(arr, cmap="inferno")
+    fig.savefig("newton.png")
     plt.show()
 
 
-x = input("Do you want to create a fractal? (y/n) \n")
-if x.lower() == "y":
-    print(f"Your function is: " + test_function["name"])
-    y = input("Choose the method: (Newton/Ostrowski/Steffensen/Halley)\n")
-    render(y, test_function["function"])
+def conv(arr):
+    count = np.count_nonzero(arr == numerical.iterations)
+    return 1 - count/(1000**2)
 
 
 
+if __name__ == "__main__":
+    x = input("Do you want to create a fractal? (y/n) \n")
+    if x.lower() == "y":
+        print(f"Your function is: " + test_function["name"])
+        y = input("Choose the method: (Newton/Ostrowski/Steffensen/Halley)\n")
+        data = render(y, test_function["function"])
+        create(data)
+        print(f"The probability of convergence of the points with {y}'s method is {conv(data)}")
+    
