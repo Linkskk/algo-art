e1f420318e1a6a3748fb961079cf2889f57d38bb
linkskk <106978598+linkskk@users.noreply.github.com>
2022-10-30 15:29:02 +0000

    Update numerical_methods.py

--- numerical_methods.py
+++ numerical_methods.py
@@ -1,100 +1,61 @@
 import cmath as c
 import numpy as np
 
+ITERATIONS = 200
+EPSILON = 1e-5
+DELTA = 1e-8
+
+
 class Numerical():
 
-
     def __init__(self):
-        self.iterations = 100
-        self.epsilon = 1e-5
-        self.delta = 1e-8
-        self.complex_delta = complex(self.delta, self.delta)
-       
-        #initalises a list of functions on which the root finding methods can work on
-           
-        self.list = [{"name": "(x^2)tanh(x) - 1", "function": lambda x: (x**2)*c.tanh(x) - 1}, 
-        {"name": "cosh(x) - 1",  "function": lambda x: c.cosh(x) - 1},
-        {"name": "cos(sin(x)) - π ",  "function": lambda x: c.cos(c.sin(x)) - c.pi},
-        {"name": "(x^3)cos(x) - 1 ",  "function": lambda x: x**3 * c.cos(x) - 1},
-        {"name": "(x^3+x^2+x)cos(sin(x))cos(x)^2-2", 
-        "function": lambda x: (x**3 + x**2 + x)*c.cos(c.sin(x))*(c.cos(x)**2) - 2},
-        {"name": "(x^2)arctan(x) - x^5 ",  "function": lambda x: x**2 * c.atan(x) - x**5}]
- 
-    
-    def derivative(self, f, x):
-        # finds the derivative of a function
-        g = lambda x: (f(x + self.complex_delta) - f(x))/ self.complex_delta
-        return g, g(x)
-    
+        self.alpha = 1
+        self.gamma = 0
+        self.mobiusmode = False
+        self.functions = \
+            [
+            {"name": "(x^2)tanh(x) - 1", "function": lambda x: (x**2)*c.tanh(x) - 1}, 
+            {"name": "cosh(x) - 1",  "function": lambda x: c.cosh(x) - 1},
+            {"name": "cos(sin(x)) - π ",  "function": lambda x: c.cos(c.sin(x)) - c.pi},
+            {"name": "(x^3)cos(x) - 1 ",  "function": lambda x: x**3 * c.cos(x) - 1},
+            {"name": "(x^3+x^2+x)cos(sin(x))cos(x)^2-2", "function": lambda x: (x**3 + x**2 + x)*c.cos(c.sin(x))*(c.cos(x)**2) - 2},
+            {"name": "(x^2)arctan(x) - x^5 ",  "function": lambda x: x**2 * c.atan(x) - x**5},
+            {"name": "x^3 - 1 ",  "function": lambda x: x**3 - 1},
+            {"name": "(x^2)ln(x) - 5",  "function": lambda x: (x**2)*c.log(x) - 5},
+            {"name": "x^8 - 1",  "function": lambda x: x**8 - x**4 - 1},
+            {"name": "i^x - 1",  "function": lambda x: (0 + 1j)**x - 1},
+            {"name": "tanh(xlog(x))",  "function": lambda x: c.tanh(x*c.log(x))},
+            {"name": "x^x - 1",  "function": lambda x: x**x - 1},
+            {"name": "x^20 - 1",  "function": lambda x: x**20 - 1}           
+            ]
 
-    def second_derivative(self, f, x):
-        # finds the second derivative of a function
-        g = self.derivative(f, x)[1]
-        h = lambda x: (g(x + self.complex_delta) - g(x))/ self.complex_delta
-        return h, h(x)
+    #returns either the derivative of a function or the value of the derivative at a specified point
+    def derivative(self, f, x):
+        g = lambda x: (f(x + complex(DELTA, DELTA)) - f(x))/ complex(DELTA, DELTA)
+        return g, g(x)
 
     def newton(self, x, f):
-        
-        for i in range(1, self.iterations + 1):
+        count = 0
+        for i in range(ITERATIONS):
             try:
                 dx = self.derivative(f, x)[1]
-                if abs(dx) < self.epsilon:
+                if abs(dx) < EPSILON:
                     break
-                x0 = x - (f(x) / dx)
-                if abs(f(x0)) < self.epsilon:
+                x0 = x - self.alpha*(f(x) / dx) + self.gamma
+                if abs(f(x0)) < EPSILON:
                     break
                 x = x0
             except OverflowError:
-                break
+                break        
         return i
 
-    def steffensen(self, x0, f):
-        for i in range(1, self.iterations + 1):
-            try:
-                x1 = f(x0)
-                x2 = f(x1)
-                delta = x1 - x0
-                delta_sqr = x0 - (2 * x1) + x2
-                if abs(delta_sqr) < self.epsilon:
-                    break
-                aitkenX = x0 - ((delta ** 2) / delta_sqr)
-                if abs(aitkenX - x2) < self.epsilon:
-                    break
-                x0 = aitkenX
-            except OverflowError:
-                break
-        return i
 
-    def ostrowski(self, x, f):
-        for i in range(1, self.iterations + 1):
-            try:
-                dx = self.derivative(f, x)[1]
-                if abs(dx) < self.epsilon:
-                    break
-                y0 = x - (f(x) / dx)
-                difference = f(x) -2*f(y0)
-                if abs(difference) < self.epsilon:
-                    break
-                x0 = y0 - f(y0)*(x - y0)/difference
-                if abs(x0 - x) < self.epsilon:
-                    break
-                x = x0
-            except OverflowError:
-                break      
-        return i
+    def mobius(self, x, a = 0, b = -1, c = 1, d = 1):
+        return (a*x + b) / (c*x + d)
 
-    def halley(self, x, f):
-        for i in range(1, self.iterations + 1):
-            try:
-                dx = self.derivative(f, x)[1]
-                ddx = self.derivative(f, x)[1]
-                if abs(dx)<self.epsilon or abs(ddx) < self.epsilon:
-                    break
-                x0 = x - (2 * f(x) * dx) / (2 * (dx) ** 2 - f(x) * ddx)
-                if abs(f(x0)) < self.epsilon:
-                    break
-                x = x0
-            except OverflowError:
-                break  
-        return i
+    #allows the user to change the parameters used in Newton's method
+    def settings(self, alpha, mobius , gamma):
+        self.alpha = alpha
+        self.gamma = gamma
+        self.mobiusmode = mobius
 
