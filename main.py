bc4b791240d6530dbcb8edc561a4f23c3355c97e
linkskk <106978598+linkskk@users.noreply.github.com>
2022-10-30 15:38:15 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,37 +1,64 @@
 import random
 import numpy as np
 import matplotlib.pyplot as plt
-from numerical_methods import Numerical 
-import PIL.Image as image
+from newton import Numerical
+from functions import functions_list
+import sys
+
+if not sys.warnoptions:
+    import warnings
+    warnings.simplefilter("ignore")
 
 
 numerical = Numerical()
-methods = {"Newton": numerical.newton, "Steffensen": numerical.steffensen, 
-            "Ostrowski": numerical.ostrowski, "Halley": numerical.halley}
-test_function = random.choice(numerical.list)
+pixels=1000
 
-def render(type, f, pixels=1500, xa=-1.5, xb=1.5, ya=-1.5, yb= 1.5):
-    print(f"Generating {type}'s fractal...")
-    x = np.linspace(xa, xb, pixels)
-    y = np.linspace(ya, yb, pixels)
-    xx, yy = np.meshgrid(x,y)
-    z = xx + yy*1j
-    im = np.frompyfunc(methods[type],2, 1)(z, f).astype(float)
-    return im
+#canvas 
+xa=-2.5
+xb=2.5
+ya=-2.5
+yb=2.5
 
-def create(arr):
-    fig = plt.figure(figsize=(1500 /200.0, 1500 / 200.0), dpi=100)
-    ax = fig.add_axes([0, 0, 1, 1], aspect=1)
-    ax.axis("off")
-    ax.imshow(arr, cmap="inferno")
-    fig.savefig("fractal.png")
-    plt.show()
+##Inputs all of the points (pixels) into Newton's method. Each point is then assigned a colour according to the number of iterations
+# required to converge to the nearest root. Finally, the points are plotted onto an grid/plane.
 
-if __name__ == "__main__":
-    x = input("Do you want to create a fractal? (y/n) \n")
-    if x.lower() == "y":
-        print(f"Your function is: " + test_function["name"])
-        y = input("Choose the method: (Newton/Ostrowski/Steffensen/Halley)\n")
-        data = render(y, test_function["function"])
-        create(data)
+def render(data, function, address):
+        print("Rendering art...")
+        im = np.frompyfunc(numerical.newton,2, 1)(data, function).astype(float)
+        fig = plt.figure(figsize=(1000 /200.0, 1000 / 200.0), dpi=200)
+        ax = fig.add_axes([0, 0, 1, 1], aspect=1)
+        ax.axis("off")
+        ax.imshow(im, cmap="gist_heat")
+        fig.savefig(address)
+
+
+print("Welcome ^_^. My name is Pixel, your local robot artist!")
+bot_is_on = True
+
+while bot_is_on:
+    art_request = input("Do you want me to make some art? If so, enter 'y'. If you want to change the settings, enter 's'. Otherwise, press 'n' to close\n")
+
+    if art_request.lower() == "y":
+        x = input("Great! Please enter the number of artworks you would like. \n")
+        if int(x) > 0:
+            for i in range(int(x)):
+                test_function = random.choice(functions_list)
+                print(f"Your function is: " + test_function["name"])
+                #generates the points which are evenly spaced
+                x = np.linspace(xa, xb, pixels)
+                y = np.linspace(ya, yb, pixels)
+                xx, yy = np.meshgrid(x,y)
+                z = numerical.mobius(xx + yy*1j) if numerical.mobiusmode else xx + yy*1j
+                render(z, test_function["function"], f"art{i}.png")
+            print("I have created all the art requested. Enjoy! ^_^")
+
+    #processes a change in the constants used in Newton's method, as well as configuring the Mobius transform
+    if art_request.lower() == "s":
+        alpha = input("Enter a new value for alpha: ")
+        beta = input("Enter a new value for beta: ")
+        mobius = input("Do you want to use the Mobius transform? Enter 'True' or 'False: ")
+        numerical.settings(complex(alpha), complex(beta), bool(mobius.title()))
+
+    if art_request.lower() == 'n':
+        bot_is_on = False
     
