7a837350bdb4d8b085f36d4b46afb470fc70ab89
linkskk <106978598+linkskk@users.noreply.github.com>
2022-11-10 16:04:46 +0000

    Add files via upload

---
+++ abstract_art.pyx
@@ -0,0 +1,76 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import cmath as c
+import sys
+
+iterations = 500
+tolerance = 1e-10
+delta = 1e-8
+
+if not sys.warnoptions:
+    import warnings
+    warnings.simplefilter("ignore")
+
+
+def derivative(x, f):
+    # cdef double delta = 1e-8
+    return (f(x + complex(delta, delta)) - f(x))/complex(delta, delta)
+
+def newton(x, f):
+    # cdef int iterations = 100
+    # cdef double tolerance = 1e-10 
+    cdef int i
+    for i in range(iterations):
+        try:
+            dx = derivative(x, f)
+            if abs(dx) < tolerance:
+                break
+            x0 = x - (f(x) / dx)
+            if abs(f(x0)) < tolerance:
+                break
+            x = x0
+            
+        except OverflowError:
+            break        
+    return i
+
+def steffensen(x0, f):
+    # cdef int iterations = 100
+    # cdef double tolerance = 1e-10 
+    cdef int i
+    for i in range(iterations):
+        try:
+            x1 = f(x0)
+            x2 = f(x1)
+            delta = x1 - x0
+            delta_sqr = x0 - (2 * x1) + x2
+            if abs(delta_sqr) < tolerance:
+                break
+            aitkenX = x0 - ((delta ** 2) / delta_sqr)
+            if abs(aitkenX - x2) < tolerance:
+                break
+            x0 = aitkenX
+        except OverflowError:
+            break
+    return i
+
+# def mobius(complex a,complex b,complex c,complex d, x):
+#     return x if abs(a*d - b*c) == 0 else (a*x + b)/(c*x + d)
+
+def generate_grid(double xa, double xb, double ya, double yb, int pixels):
+    x = np.linspace(xa, xb, pixels)
+    y = np.linspace(ya, yb, pixels)
+    xx, yy = np.meshgrid(x,y)
+    return xx + yy*1j
+
+def render(function, address):
+    data = generate_grid(1, -1, 1, -1, 1000)
+    im = np.frompyfunc(newton,2, 1)(data, function).astype(float)
+    fig = plt.figure(figsize=(1000 /200.0, 1000 / 200.0), dpi=500)
+    ax = fig.add_axes([0, 0, 1, 1], aspect=1)
+    ax.axis("off")
+    ax.imshow(im, cmap="gist_gray")
+    fig.savefig(address)
+
+
+
---
+++ main.py
@@ -0,0 +1,10 @@
+import abstract_art
+import time
+import cmath as c
+
+test_function = lambda x: c.cos(c.sin(x)) - c.pi
+st = time.time()
+abstract_art.render(test_function, "image_iv.png")
+et = time.time()
+
+print(f"Time to create art: {et - st} seconds")
