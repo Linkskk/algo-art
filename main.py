249ef4c1f37a1846cc3d41eefea0fdc5c51a9a06
linkskk <106978598+linkskk@users.noreply.github.com>
2022-11-23 14:30:36 +0000

    Update main.py

--- main.py
+++ main.py
@@ -2,9 +2,8 @@ import abstract_art
 import time
 import cmath as c
 
-test_function = lambda x: c.cos(c.sin(x)) - c.pi
 st = time.time()
-abstract_art.render(test_function, "abstract_art.png")
+abstract_art.render("art.png")
 et = time.time()
 
 print(f"Time to create art: {et - st} seconds")
